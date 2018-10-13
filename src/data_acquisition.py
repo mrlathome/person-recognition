d40f094777e7584675fc53c93bb121268ee13507
Mohammad Javad Azadi <mjazadix01@gmail.com>
2018-10-13 10:24:16 +0330

    Added the data acquisition module along with the related test case

---
+++ src/data_acquisition.py
@@ -0,0 +1,16 @@
+"""
+Acquires data from multiple sources
+"""
+
+import numpy as np
+
+
+def load_img(file):
+    """
+    Reads image from disk
+    :param file: image file path
+    :return: RGB image as numpy array
+    """
+    # Creating an empty array only to see the test fail
+    image = np.array([])
+    return image
