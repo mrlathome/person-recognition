6be13624d8b628acbebd4290588b26db340225d4
Pooya Kooshanfar <pooyakoshanfsr@gmail.com>
2018-10-13 15:57:01 +0330

    completing data processing test module

---
+++ src/data_processing.py
@@ -0,0 +1,13 @@
+"""
+process data and produce valid output for other moduls
+"""
+
+
+def transform(data):
+    pass
+
+def preprocess(data):
+    pass
+
+def encode(preprcessed_data):
+    return [[1],[2]]
---
+++ src/data_processing_test.py
@@ -0,0 +1,20 @@
+"""
+Test cases for data preprocessing modul
+"""
+import cv2
+import unittest
+import tensorflow as tf
+
+from data_processing import encode
+
+
+class DataPreprocessingTestCase(unittest.TestCase):
+
+    def test_encode(self):
+        preprocessed_data = cv2.imread('images/andrew.jpg')
+        encoded_image = encode(preprocessed_data)
+        self.assertEqual(len(encoded_image[0]) == 128, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
