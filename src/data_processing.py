e7b629348d28cc199ca3cad3333912d8b0c8a399
Pooya Kooshanfar <pooyakoshanfsr@gmail.com>
2018-10-30 14:57:54 +0330

    Finished detect face function and added aling file

--- src/data_processing.py
+++ src/data_processing.py
@@ -4,11 +4,15 @@ process data and produce valid output for other moduls
 
 import sys
 import tensorflow as tf
-import os
-import cv2
 import align.detect_face
 
+
 class DataProcessing:
+
+    pnet_threshold = .6
+    rnet_threshold = .7
+    onet_threshold = .7
+
     def __init__(self, pkg_dir):
         self.pkg_dir = pkg_dir
 
@@ -19,13 +23,10 @@ class DataProcessing:
         :param image: The input image
         :return: A list of bounding boxes
         """
-        sys.path.append('/home/pooya/Documents/code/facenet-master/src')
         gpu_memory_fraction = 1.0
         minsize = 50
-        threshold = [0.6, 0.7, 0.7]
+        threshold = [DataProcessing.pnet_threshold, DataProcessing.rnet_threshold, DataProcessing.onet_threshold]
         factor = 0.709
-        image_dir = ''
-        images = os.listdir(image_dir)
         with tf.Graph().as_default():
             gpu_options = tf.GPUOptions(
                 per_process_gpu_memory_fraction=gpu_memory_fraction)
@@ -34,11 +35,9 @@ class DataProcessing:
             with sess.as_default():
                 pnet, rnet, onet = align.detect_face.create_mtcnn(
                     sess, None)
-            for i in images:
-                img = cv2.imread(os.path.expanduser(image_dir + i))
                 bounding_boxes, _ = align.detect_face.detect_face(
-                    img, minsize, pnet,
+                    image, minsize, pnet,
                     rnet, onet, threshold, factor)
+        x1, y1, x2, y2, acc = bounding_boxes[0]
+        return [int(x1), int(y1), int(x2), int(y2)]
 
-
-        return [int(x1),int(y1),int(x1 + w),int(y1 + h)]
